930345f5209c6f380d40bfc225bcafc0ac90eedd
noahgould <gould.noah@gmail.com>
2018-04-13 22:18:29 -0600

    Cleaned wikipedia controller a bit.

--- api/wikipediaController.go
+++ api/wikipediaController.go
@@ -1,4 +1,4 @@
-package main
+package api
 
 import (
 	"encoding/json"
@@ -10,14 +10,7 @@ import (
 	"time"
 )
 
-type Artist struct {
-	Name    string
-	City    string
-	State   string
-	Country string
-}
-
-type Page struct {
+type page struct {
 	PageID    int    `json:"pageid"`
 	Ns        int    `json:"ns"`
 	Title     string `json:"title"`
@@ -28,10 +21,10 @@ type Page struct {
 	}
 }
 
-type WikiApiResult struct {
+type wikiApiResult struct {
 	BatchComplete string `json:"batchcomplete"`
 	Query         struct {
-		Pages map[string]*Page
+		Pages map[string]*page
 	}
 }
 
@@ -64,7 +57,7 @@ func LookupArtistLocation(artist string) string {
 		log.Fatal(readErr)
 	}
 
-	pageInfo := WikiApiResult{}
+	pageInfo := wikiApiResult{}
 	jsonErr := json.Unmarshal(body, &pageInfo)
 
 	if jsonErr != nil {
@@ -87,55 +80,4 @@ func LookupArtistLocation(artist string) string {
 	location = strings.Replace(location, "[", "", -1)
 	location = strings.Replace(location, "&nbsp;", " ", -1)
 	return location
-}
-
-func main() {
-	artist := "The%20Shins"
-	url := "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + artist + "&rvsection=0"
-
-	wikiClient := http.Client{
-		Timeout: time.Second * 2,
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Set("User-Agent", "bandsfromtown")
-
-	res, getErr := wikiClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	pageInfo := WikiApiResult{}
-	jsonErr := json.Unmarshal(body, &pageInfo)
-
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-
-	var infoBoxItems []string
-	for _, p := range pageInfo.Query.Pages {
-		if strings.Contains(p.Revisions[0].Content, "origin =") {
-			infoBoxItems = strings.Split(p.Revisions[0].Content, "origin =")
-		} else if strings.Contains(p.Revisions[0].Content, "birth_place =") {
-			infoBoxItems = strings.Split(p.Revisions[0].Content, "birth_place =")
-		} else {
-			fmt.Println("No birthplace/origin data")
-		}
-	}
-	endOfLocation := strings.Index(infoBoxItems[1], "|")
-	location := strings.Trim(infoBoxItems[1][0:endOfLocation], " ")
-	location = strings.Replace(location, "]", "", -1)
-	location = strings.Replace(location, "[", "", -1)
-	location = strings.Replace(location, "&nbsp;", " ", -1)
-	fmt.Println(location)
-
 }
