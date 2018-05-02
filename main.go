bc48d904eab422c5a68fb27a20b99ac5ed252d9f
noahgould <gould.noah@gmail.com>
2018-05-01 23:57:15 -0600

    routes are hard.

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 	r.HandleFunc("/artist", artistController.Index)
 	r.PathPrefix("/frontend/").Handler(http.StripPrefix("/frontend/", http.FileServer(http.Dir("frontend"))))
 	r.Handle("/", http.RedirectHandler("/frontend/artistLookup.html", 301))
+	r.HandleFunc("/spotify/auth", spotifyController.AuthorizationRequest)
 	r.HandleFunc("/spotify/login", spotifyController.AuthorizationCallback)
 	r.HandleFunc("/spotify/{spotifyID}", spotifyController.MapUserArtists)
 
