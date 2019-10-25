b81df71a39ed00cd58b0ed3664dc367c05e49af1
noahgould <noahgould@users.noreply.github.com>
2019-10-25 00:05:46 -0700

    Move to psql (#32) (#33)
    * use spotify redirect url from env vars.
    
    * Use pgx for psql. Change queries. Catch some bugs. Add migrations to ensure db stays up to date. Move to go modules.
    
    * move gmaps api key to env var.

--- api/spotifyController.go
+++ api/spotifyController.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"database/sql"
 	"encoding/json"
 	"html/template"
 	"log"
@@ -10,6 +9,7 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/jackc/pgx"
 
 	"github.com/noahgould/bandsfromtown/dal"
 )
@@ -184,7 +184,7 @@ func (sc *SpotifyController) checkSavedWithSpotify(artists <-chan dal.Artist, re
 		for artist := range artists {
 			existingArtist, err := sc.artistStore.GetArtistBySpotifyID(artist.SpotifyID)
 			if err != nil {
-				if err != sql.ErrNoRows {
+				if err != pgx.ErrNoRows {
 					log.Println(err)
 				} else {
 					notSaved <- artist
@@ -210,7 +210,7 @@ func (sc *SpotifyController) checkSavedByName(artists <-chan dal.Artist, readyAr
 		for artist := range artists {
 			existingArtists, err := sc.artistStore.GetArtistsByName(artist.Name)
 			if err != nil {
-				if err != sql.ErrNoRows {
+				if err != pgx.ErrNoRows {
 					log.Println(err)
 				} else {
 					notSavedArtists <- artist
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/noahgould/bandsfromtown
+
+go 1.13
+
+require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/golang-migrate/migrate/v4 v4.7.0
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/gorilla/mux v1.7.3
+	github.com/jackc/pgx v3.6.0+incompatible
+	github.com/jackc/pgx/v4 v4.1.1
+	github.com/sergi/go-diff v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
+	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	googlemaps.github.io/maps v0.0.0-20191014172202-ce2e58e026c5
+)
